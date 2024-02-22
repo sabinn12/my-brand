b1d75408917973eb4f29a59e2d5c868b283e2ad7
sabinn12 <irakizasabin12@gmail.com>
2024-02-22 14:50:34 +0200

    updates

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Home or main content navbar */ 
 let menuBtn = document.getElementById("menu");
 let links = document.getElementById("links");
 
@@ -5,3 +6,16 @@ menuBtn.addEventListener("click", function(e){
     links.classList.toggle('responsive');
     menuBtn.classList.toggle('fa-x')
 });
+
+/* dashboard navbar */
+
+
+function openNav() {
+    document.getElementById("side").style.width = "230px";
+    document.getElementById("main").style.marginLeft = "230px";
+  }
+  
+  function closeNav() {
+    document.getElementById("side").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }

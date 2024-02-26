d9efc4379cb6b6a35d504c6c1550fe2a017d16c3
sabinn12 <irakizasabin12@gmail.com>
2024-02-26 09:07:53 +0200

    updates

--- script.js
+++ script.js
@@ -6,6 +6,28 @@ menuBtn.addEventListener("click", function(e){
     links.classList.toggle('responsive');
     menuBtn.classList.toggle('fa-x')
 });
+// blog sliders
+var slider = document.getElementById('slider');
+        var slides = Array.from(slider.children);
+        var slideWidth = slides[0].getBoundingClientRect().width;
+        var slideIndex = 0; // Define slideIndex globally
+
+        function nextSlide() {
+            slideIndex = (slideIndex + 1) % slides.length;
+            slider.scrollTo({
+                left: slideWidth * slideIndex,
+                behavior: 'smooth'
+            });
+        }
+
+        function prevSlide() {
+            slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+            slider.scrollTo({
+                left: slideWidth * slideIndex,
+                behavior: 'smooth'
+            });
+        }
+
 
 /* dashboard navbar */
 
